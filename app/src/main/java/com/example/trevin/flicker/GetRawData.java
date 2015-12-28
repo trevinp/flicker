4b5471fc36370e44d69e1b7988a94fe75410bdc5
Trevin Pick <trevinp@gmail.com>
2015-12-27 20:48:20 -0800

    Initial

---
+++ app/src/main/java/com/example/trevin/flicker/GetRawData.java
@@ -0,0 +1,17 @@
+package com.example.trevin.flicker;
+
+/**
+ * Created by Trevin on 12/27/2015.
+ */
+enum DownloadStatus{IDLE, PROCESSING, NOT_INITIALIZED, FAILED_OR_EMPTY, OK}
+
+public class GetRawData {
+    private String LOG_TAG = GetRawData.class.getSimpleName();
+    private String mRawUrl;
+    private String mData;
+    private DownLoadStatus mdownloadStatus;
+
+    public GetRawData[String mRawUrl] {
+        this.mRawUrl = mRawUrl;
+    }
+}
